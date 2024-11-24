c83631f86c9b968c91392e7a3dc29116e0b5e1f8
KachusovStepan <stepanurfu@gmail.com>
2024-11-24 14:17:57 +0500

    feat: added taxonomic diversity form

--- libs/main/preview/src/lib/flora/taxonomic/taxonomic.component.ts
+++ libs/main/preview/src/lib/flora/taxonomic/taxonomic.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-taxonomic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './taxonomic.component.html',
   styleUrl: './taxonomic.component.css',
 })
-export class TaxonomicComponent {}
+export class TaxonomicComponent {
+
+  searchForm = new FormGroup({
+    family: new FormControl(''),
+    genus: new FormControl(''),
+    species: new FormControl(''),
+  });
+
+  handleSubmit() {
+
+  }
+}
